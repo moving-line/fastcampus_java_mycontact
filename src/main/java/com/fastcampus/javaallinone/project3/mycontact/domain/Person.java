e31399e457af63838993e24d66f594a4c4ec484b
moving-line <movinglinecheck@gmail.com>
2020-02-06 19:27:47 +0900

    Initial commit
    - Spring Initializr 를 이용한 프로젝트 생성
    - spring-boot-starter-web, spring-boot-starter-jpa(with h2database), lombok 의존 설정
    - 매우 간단한 test 진행

---
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -0,0 +1,41 @@
+package com.fastcampus.javaallinone.project3.mycontact.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private Integer age;
+
+    private String hobby;
+
+    @NonNull
+    private String bloodType;
+
+    private String address;
+
+    private LocalDate birthday;
+
+    private String job;
+
+    @ToString.Exclude
+    private String phoneNumber;
+
+
+}
