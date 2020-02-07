0497e129d5bb91f6aec261369c221d49d6683231
moving-line <movinglinecheck@gmail.com>
2020-02-07 11:19:20 +0900

    Add jpa relation
    - Person과 Block을 OneToOne으로 연결하고 관련 옵션 테스트

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -2,9 +2,7 @@ package com.fastcampus.javaallinone.project3.mycontact.domain;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
@@ -37,5 +35,7 @@ public class Person {
     @ToString.Exclude
     private String phoneNumber;
 
-
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    private Block block;
 }
