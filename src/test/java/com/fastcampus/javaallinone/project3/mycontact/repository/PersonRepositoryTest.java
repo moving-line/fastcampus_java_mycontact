9ba60319acae78bff77490d3575fb526a13c3b7e
moving-line <movinglinecheck@gmail.com>
2020-02-10 14:16:23 +0900

    Refactor repository test
    - Respoistory단의 모든 test 개선

--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -1,45 +1,65 @@
 package com.fastcampus.javaallinone.project3.mycontact.repository;
 
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
+import com.fastcampus.javaallinone.project3.mycontact.domain.dto.Birthday;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 @Transactional
 class PersonRepositoryTest {
+
     @Autowired
     private PersonRepository personRepository;
 
     @Test
-    void crud() {
-        Person person = new Person();
-        person.setName("john");
-
-        personRepository.save(person);
-
-        List<Person> people = personRepository.findByName("john");
+    void findByName() {
+        List<Person> people = personRepository.findByName("tony");
+        Person person = people.get(0);
 
         assertThat(people.size()).isEqualTo(1);
-        assertThat(people.get(0).getName()).isEqualTo("john");
-//        assertThat(people.get(0).getAge()).isEqualTo(10);
+        assertAll(
+                () -> assertThat(person.getName()).isEqualTo("tony"),
+                () -> assertThat(person.getHobby()).isEqualTo("reading"),
+                () -> assertThat(person.getAddress()).isEqualTo("서울"),
+                () -> assertThat(person.getBirthday()).isEqualTo(Birthday.of(LocalDate.of(1991, 7, 10))),
+                () -> assertThat(person.getJob()).isEqualTo("officer"),
+                () -> assertThat(person.getPhoneNumber()).isEqualTo("010-2222-5555"),
+                () -> assertThat(person.isBirthdayToday()).isEqualTo(false)
+        );
     }
 
     @Test
-    void findByBirthdayBetween() {
-        List<Person> result = personRepository.findByMonthOfBirthday(8);
+    void findByNameIfDeleted() {
+        List<Person> people = personRepository.findByName("andrew");
 
-//        Person martin = new Person();
-//        martin.getAge();
-//        martin.isBirthdayToday();
+        assertThat(people.size()).isEqualTo(0);
+    }
 
-        assertThat(result.size()).isEqualTo(2);
-        assertThat(result.get(0).getName()).isEqualTo("martin");
-        assertThat(result.get(1).getName()).isEqualTo("sophia");
+    @Test
+    void findByMonthOfBirthday() {
+        List<Person> people = personRepository.findByMonthOfBirthday(7);
+
+        assertThat(people.size()).isEqualTo(2);
+        assertAll(
+                () -> assertThat(people.get(0).getName()).isEqualTo("david"),
+                () -> assertThat(people.get(1).getName()).isEqualTo("tony")
+        );
+    }
+
+    @Test
+    void findPeopleDeleted() {
+        List<Person> people = personRepository.findPeopleDeleted();
+
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("andrew");
     }
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -3,3 +3,7 @@ INSERT INTO Person(id, name, year_of_birthday, month_of_birthday, day_of_birthda
 INSERT INTO Person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) VALUES (3, 'dennis', 1993, 10, 15)
 INSERT INTO Person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) VALUES (4, 'sophia', 1994, 8, 31)
 INSERT INTO Person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) VALUES (5, 'benny', 1995, 12, 23)
+
+INSERT INTO Person(id, name, year_of_birthday, month_of_birthday, day_of_birthday, job, hobby, phone_number, address) VALUES (6, 'tony', 1991, 7, 10, 'officer', 'reading', '010-2222-5555', '서울')
+INSERT INTO Person(id, name, deleted) VALUES (7, 'andrew', true)
+
